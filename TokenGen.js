7996a98140a0bda361bfa5f46156bb31d4b2807e
Dokoye98 <Dwayneokoye98@gmail.com>
2024-10-02 14:35:02 +0100

    user endpoints are completed and optimised

---
+++ TokenGen.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken')
+
+function authenticate(req,res,next){
+    const authHeader = req.header('Authorization')
+    const token = authHeader && authHeader.split(' ')[1]
+    if(!token){
+        return res.status(401).send({message:'Access Denied'})
+    }
+    try{
+        const verified = jwt.verify(token,process.env.TOKEN)
+        req.user = verified
+        next()
+    }catch(err){
+        return res.status(401).send({ message: 'Invalid Token' })
+    }
+}
--- routes/User.js
+++ routes/User.js
@@ -4,3 +4,53 @@ const router = express()
 const {signUpValidation,loginValidation} = require('../validations/valid')
 const bcryptjs = require('bcryptjs')
 const jsonwebtoken = require('jsonwebtoken')
+
+router.post('/signup',async(req,res)=>{
+    const {error} = signUpValidation(req.body)
+    if(error){
+        console.log({message:error['details'][0]['message']})
+        return res.status(400).send({message:error['details'][0]['message']}) 
+    }
+    const emailCheck = await User.findOne({email:req.body.email})
+    const userCheck = await User.findOne({username:req.body.username})
+    if(emailCheck || userCheck){
+        return res.status(400).send({message:'Email or Username is already in use'})
+    }
+    const salt = await bcryptjs.genSalt(10)
+    const hashpassword = await bcryptjs.hash(req.body.password,salt)
+    const dataFormat = new User({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        username: req.body.username,
+        email: req.body.email,
+        password: hashpassword
+    })
+    try{
+        const newUser = await dataFormat.save()
+        console.log('new user added')
+        return res.status(200).send({message:'SignUp complete',newUser})
+    }catch(error){
+        return res.status(400).send({message:error})
+    }
+})
+
+router.post('/login',async(req,res)=>{
+    const {error} = loginValidation(req.body)
+    if(error){
+        console.log({message:error['details'][0]['message']})
+        return res.status(400).send({message:error['details'][0]['message']}) 
+    }
+    const userCheck = await User.findOne({
+        $or:[{username:req.body.username},{email:req.body.email}]
+    })
+
+    if(!userCheck){
+        return res.status(400).send({message:'Account does not exist'})
+    }
+    const passwordCheck  = await bcryptjs.compare(req.body.password,userCheck.password)
+    if(!passwordCheck){
+        return res.status(400).send({message:'Incorrect password'})
+    }
+    const accessToken = jsonwebtoken.sign({_id:userNameCheck._id},process.env.TOKEN, { expiresIn: '1h' })
+    res.header('auth-token',accessToken).send({ 'auth-token': accessToken })
+})
